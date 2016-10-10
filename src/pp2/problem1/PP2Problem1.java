3c65abe1bbaec00caf687dbbf5b6f8f59cd3f7d4
PC <PC@HP>
2016-10-10 17:46:23 -0500

    1. Realizar un programa que indique si es año bisiesto o no.

--- src/pp2/problem1/PP2Problem1.java
+++ src/pp2/problem1/PP2Problem1.java
@@ -15,9 +15,9 @@ public class PP2Problem1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-    verificarAño(solicitarDatos("año"));
+    verificarAño(solicitarDatos("año")); //Se solicita el año y mediante el metodo verficiarAño se obtiene el resultado
     }
-    public static int solicitarDatos(String d){
+    public static int solicitarDatos(String d){ //Solicita datos
         int miDato;
         Scanner entradaEscaner = new Scanner(System.in);
         System.out.print("Introduce el dato " + d);
@@ -25,12 +25,14 @@ public class PP2Problem1 {
         return miDato;
     }
     public static void verificarAño(int a){
-        if (a % 4 == 0 && a % 100 != 0 || a % 400 == 0){
-              muestraVeredicto("es bisiesto");
-    }else
-              muestraVeredicto("no es bisiesto");
+        if (a % 4 == 0 && a % 100 != 0 || a % 400 == 0){ //Si el residuo de la division del año entre 4 es igual a 0
+                                                         // y si el residuo de la division del año entre 100 es diferente de 0
+                                                         // o si el residuo de la division del año entre 400 es igual a 0
+              muestraVeredicto("es bisiesto"); //Se muestra que es bisiesto
+    }else                                      //sino
+              muestraVeredicto("no es bisiesto"); //Se muestra que no es bisiesto
     }
-    public static void muestraVeredicto(String d){
+    public static void muestraVeredicto(String d){ //Metodo para mostrar el resultado de la verificacion
         System.out.print ("El año " + d);
     }
 }
